e8eae4036e1b089a3788111cd18944114ad47b86
user <user@hidden.com>
2021-09-19 22:21:48 +0200

    Reintroduces the simulator in vue.js

--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
+if [ -n "$FORCE_BUILD" ]; then
+    USER="$(id -u):$(id -g)" docker-compose build "$@"
+fi    
+
 USER="$(id -u):$(id -g)" docker-compose up "$@"
--- frontend/src/Simulator.vue
+++ frontend/src/Simulator.vue
@@ -1,21 +1,49 @@
 <template>
-    <div class="container-fluid">
-      <div class="row">
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-          <h1>Simulator:</h1>
-          <h2>{{ sign }}</h2>
-        </main>
-      </div>
+  <Fragment>
+    <div id="content">
+        <canvas id="application" style="height: 100%; width: 100%;"></canvas>
     </div>
+    <footer>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Sequences" active>
+          <Sequences v-bind:sign="sign"></Sequences>
+        </b-tab>
+        <b-tab title="Bulbs">
+          <p>Bulb selection interface will be here</p>
+        </b-tab>
+        <b-tab title="Development">
+          <p>WebSocket development mode will be here</p>
+        </b-tab>
+        <b-tab title="Disabled" disabled>
+          <p>I'm a disabled tab!</p>
+        </b-tab>
+      </b-tabs>
+    </footer>
+  </Fragment>
 </template>
 
 <script>
+  import { init, loadSign, pushFrame } from './simulator/sim.js';
+  import { Fragment } from 'vue-fragment'
+  import Sequences from './components/Sequences.vue'
+
   export default {
     name: 'simulator',
-    components: {
+    components: { 
+      Fragment, 
+      Sequences, 
     },
     props: {
       sign: String
+    },
+    mounted() {
+      const api_base = "api/signs/" + this.sign;
+
+      // create a PlayCanvas application
+      const canvas = document.getElementById('application');
+      init(canvas);
+
+      loadSign(api_base);
     },
     data() {
       return {
@@ -25,5 +53,30 @@
 </script>
 
 <style>
-    
+      html,
+  body {
+      height: 100%;
+  }
+
+  header {
+      position: fixed;
+      width: 100%;
+      top: 0;
+      padding: 1rem 0;
+  }
+
+  #content {
+      padding-bottom: 16em;
+      height: 100%;
+      width: 100%;
+  }
+
+  footer {
+      position: fixed;
+      height: 16em;
+      width: 100%;
+      bottom: 0;
+      line-height: 3rem;
+      overflow-y: auto;
+  }
 </style>
